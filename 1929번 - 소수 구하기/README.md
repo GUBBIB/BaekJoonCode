9c704159a7b36f8bb2c1cfd2f8b799c001c19857
GUBBIB <moon.dragon250@gmail.com>
2024-11-09 00:42:29 +0900

    Update

---
+++ 1929번 - 소수 구하기/README.md
@@ -0,0 +1,35 @@
+<!-- ChatGPT를 사용하여 꾸몄습니다 -->
+# <img src="https://github.com/GUBBIB/BaekJoonCode/blob/main/Tier_Img/Silver-3svg" alt="티어그림" width="30px" height="35px"> [1929번 - 소수 구하기](https://www.acmicpc.net/problem/1929)
+
+<h2>📝 문제 설명</h2>
+<div style="background-color: #f9f9f9; padding: 10px; border-radius: 5px; line-height: 1.6;">
+    10,000 이하의 자연수로 이루어진 길이 N짜리 수열이 주어진다. 이 수열에서 연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중, 가장 짧은 것의 길이를 구하는 프로그램을 작성하시오.
+</div>
+
+<h2>📥 입력</h2>
+<div style="background-color: #f0f8ff; padding: 10px; border-radius: 5px; line-height: 1.6;">
+    첫째 줄에 N (10 ≤ N < 100,000)과 S (0 < S ≤ 100,000,000)가 주어진다. 둘째 줄에는 수열이 주어진다. 수열의 각 원소는 공백으로 구분되어져 있으며, 10,000이하의 자연수이다.
+</div>
+
+<h2>📤 출력</h2>
+<div style="background-color: #f0fff0; padding: 10px; border-radius: 5px; line-height: 1.6;">
+    첫째 줄에 구하고자 하는 최소의 길이를 출력한다. 만일 그러한 합을 만드는 것이 불가능하다면 0을 출력하면 된다.
+</div>
+
+<h2>💡 예제</h2>
+
+```plaintext
+예제 입력 1
+3 16
+
+에제 출력 1
+3
+5
+7
+11
+13
+```
+
+## ✅ 정답 코드
+**정답 코드**:
+- [정답 코드/1929번 - Main.java]()
---
+++ 1929번 - 소수 구하기/정답코드/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[] prime = new boolean[m+1];
+
+        prime[0] = prime[1] = true;
+
+        for(int i=2; i<=Math.sqrt(m); i++){
+            if(prime[i]) {
+                continue;
+            }
+
+            for(int j=i*i; j<=prime.length; j=j+i){
+                // j = j + i; 이기 때문에 i를 더할 때 m을 넘어가는 경우가 있음
+                // 3 20 일 때 j = 18 시점이 끝나고 그 다음 값이 21이 기때문에 그 때 IndexBound가 난다
+                if(j > m) continue;
+                prime[j] = true;
+            }
+        }
+
+        for(int i=n; i<=m; i++){
+            if(!prime[i]) sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
