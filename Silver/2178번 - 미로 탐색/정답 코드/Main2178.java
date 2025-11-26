08aaaee5743829dc806445768e31b49a2b5b9218
GUBBIB <moon.dragon250@gmail.com>
2025-11-27 01:04:43 +0900

    2178번 추가

---
+++ Silver/2178번 - 미로 탐색/정답 코드/Main2178.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main2178 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N + 1][M + 1];
+        int[][] dist = new int[N + 1][M + 1];
+        int[][] arr = new int[N + 1][M + 1];
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        for(int i=1; i<=N; i++){
+            char[] str = br.readLine().toCharArray();
+            for(int j=1; j<=M; j++){
+                arr[i][j] = Integer.parseInt(String.valueOf(str[j-1]));
+            }
+        }
+
+        q.offer(new int[]{1, 1});
+        visited[1][1] = true;
+        dist[1][1] = 1;
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int x = current[0];
+            int y = current[1];
+
+            for(int d=0; d<4; d++){
+                int cx = x + dx[d];
+                int cy = y + dy[d];
+
+                if(cx < 1 || cy < 1 || cx > N || cy > M ) continue; // 범위 초과
+                if(arr[cx][cy] == 0) continue; // 벽
+                if(visited[cx][cy]) continue; // 방문함
+
+                visited[cx][cy] = true;
+                dist[cx][cy] = dist[x][y] + 1;
+                q.offer(new int[]{cx, cy});
+            }
+        }
+
+        sb.append(dist[N][M]);
+
+        System.out.println(sb);
+    }
+
+}
--- Silver/3273번 - 두 수의 합/정답 코드/Main3273.java
+++ Silver/3273번 - 두 수의 합/정답 코드/Main3273.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main3273 {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
